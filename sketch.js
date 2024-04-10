878a0bbfccb62f587d689b4f0323a5aa3ee5bde3
Jonas Oliveira <regiki@hotmail.com>
2024-04-10 10:36:42 -0300

    update

--- sketch.js
+++ sketch.js
@@ -4,10 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	
-}
+
 
 function setup() {
 	createCanvas(800, 700);
@@ -17,8 +14,30 @@ function setup() {
 	world = engine.world;
 
 	//Crie os Corpos Aqui.
+var engine, world;
+var block1, block2, block, plane;
 
+function setup(){
+	var canvas = createCanvas(550, 600);
 
+	engine = Engine.create();
+	world = engine.world;
+
+	var plane_options = {
+		isStatic: true
+	}
+
+}
+
+plane = Bodies.rectangle(600, 580, 1200, 2, plane_options);
+World.add(world, plane);
+
+var block1_options ={
+	restituiton: 0.5,
+	friction: 0.02,
+	frictionAir: 0
+
+}
 	Engine.run(engine);
   
 }
